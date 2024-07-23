79d91e613770cd758a8b8e39e68654141be7657e
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-07-23 11:51:35 -0400

    register

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Authorized } from "./Auth"
 import { Login } from "../pages/Login.jsx"
+import { Register } from '../pages/Register.jsx'
 
 
 export const App = () => {
@@ -11,6 +12,8 @@ export const App = () => {
     <BrowserRouter>
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
             <Route element={<Authorized />}>
                 
             </Route>
