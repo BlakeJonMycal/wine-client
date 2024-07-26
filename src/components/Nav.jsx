b76bac0c5cda3bf7ac73e42f883611e4919a2bb4
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-07-26 15:37:22 -0400

    logout updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,35 +4,44 @@ import "./Nav.css"
 export const NavBar = () => {
   const navigate = useNavigate()
 
-  return <ul className="navbar">
-    <li className="navbar-item">
-      <Link to="/mywines">My Cellar</Link>
-    </li>
-    <li className="navbar-item">
-      <Link to="/allwines">Explore Wines</Link>
-    </li>
-    <li className="navbar-item">
-      <Link to="/addwine">Add A Wine</Link>
-    </li>
-    <li className="navbar-item">
-      <Link to="/profile">My Profile</Link>
-    </li>
-    {localStorage.getItem("wine_token") ? (
+  const handleLogout = () => {
+    localStorage.removeItem("wine_token")
+    navigate("/login", { replace: true })
+  }
+
+  return (
+    <ul className="navbar">
+      <li className="navbar-item">
+        <Link to="/mywines" className="navbar-link">My Cellar</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/allwines" className="navbar-link">Explore Wines</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/addwine" className="navbar-link">Add A Wine</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/profile" className="navbar-link">My Profile</Link>
+      </li>
+      {localStorage.getItem("wine_token") ? (
         <li className="navbar-item navbar-logout">
-          <button
+          <span
             className="navbar-link"
-            onClick={() => {
-              localStorage.removeItem("wine_token");
-              navigate("/login", { replace: true, state: { resetForm: true } });
+            onClick={handleLogout}
+            role="button"
+            tabIndex={0}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                handleLogout()
+              }
             }}
           >
             Logout
-          </button>
+          </span>
         </li>
-    ) : (
-      ""
-    )}
-    <img className="logo" src="/images/capstone logo.png" alt="capstone logo" />
-
-  </ul>
+      ) : null}
+      <img className="logo" src="/images/capstone logo.png" alt="capstone logo" />
+    </ul>
+  )
 }
+
